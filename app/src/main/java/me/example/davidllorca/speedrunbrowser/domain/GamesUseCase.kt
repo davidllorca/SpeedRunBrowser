8ab150285dd5a723ff0c47c05efa05a29e66e7c0
David Llorca Baron <david.llorca@mango.com>
2018-08-05 18:29:30 +0200

    Set threading configuration for UseCases

--- app/src/main/java/me/example/davidllorca/speedrunbrowser/domain/GamesUseCase.kt
+++ app/src/main/java/me/example/davidllorca/speedrunbrowser/domain/GamesUseCase.kt
@@ -1,19 +1,24 @@
 package me.example.davidllorca.speedrunbrowser.domain
 
+import io.reactivex.Scheduler
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import me.example.davidllorca.speedrunbrowser.data.remote.SpeedRunApi
 import me.example.davidllorca.speedrunbrowser.domain.common.BaseUseCase
 import me.example.davidllorca.speedrunbrowser.domain.common.NoParams
 import me.example.davidllorca.speedrunbrowser.domain.model.Game
 import me.example.davidllorca.speedrunbrowser.domain.model.toGame
 
-class GamesUseCase(private val api: SpeedRunApi) : BaseUseCase<NoParams, List<Game>> {
+class GamesUseCase(
+        private val api: SpeedRunApi,
+        private val workScheduler: Scheduler,
+        private val deliveryScheduler: Scheduler)
+    : BaseUseCase<NoParams, List<Game>> {
 
     override fun execute(params: NoParams): Single<List<Game>> {
         return api.getGames()
                 .map { response -> response.gameItems?.toGame() ?: emptyList() }
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(workScheduler)
+                .observeOn(deliveryScheduler)
     }
 
 }
--- app/src/main/java/me/example/davidllorca/speedrunbrowser/domain/di/DomainModule.kt
+++ app/src/main/java/me/example/davidllorca/speedrunbrowser/domain/di/DomainModule.kt
@@ -2,14 +2,29 @@ package me.example.davidllorca.speedrunbrowser.domain.di
 
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import me.example.davidllorca.speedrunbrowser.data.remote.SpeedRunApi
 import me.example.davidllorca.speedrunbrowser.domain.GamesUseCase
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class DomainModule {
 
+    @Singleton
     @Provides
     fun getGamesUseCase(api: SpeedRunApi): GamesUseCase {
-        return GamesUseCase(api)
+        return GamesUseCase(api, workerScheduler(), deliveryScheduler())
     }
+
+    @Provides
+    @Named("worker_scheduler")
+    fun workerScheduler(): Scheduler = Schedulers.io()
+
+    @Provides
+    @Named("delivery_scheduler")
+    fun deliveryScheduler(): Scheduler = AndroidSchedulers.mainThread()
+
 }
